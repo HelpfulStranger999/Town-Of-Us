6fba29fff4d1e31c6717c0aa782b6ff40c3cd478
Helpful Stranger999 <25759101+HelpfulStranger999@users.noreply.github.com>
2021-06-24 15:40:07 -0500

    Work on voting

---
+++ source/Services/VoteService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TownOfUs.Events;
+using TownOfUs.Extensions;
+using UnityEngine;
+
+namespace TownOfUs.Services
+{
+    public class VoteService
+    {
+        public event MeetingStartEventHandler? MeetingStarted;
+
+        public event MeetingFrameUpdateEventHandler? MeetingFrameUpdated;
+
+        public event MeetingEndEventHandler? MeetingEnded;
+
+        public event VotingStartEventHandler? VotingStarted;
+
+        public event VoteSelectEventHandler? VoteSelected;
+
+        public event VoteClearEventHandler? VoteCleared;
+
+        public event VoteConfirmEventHandler? VoteConfirmed;
+
+        public event VotingCompleteEventHandler? VotingCompleted;
+
+        private RoleManager RoleManager { get; }
+
+        public VoteService(RoleManager roleManager)
+        {
+            RoleManager = roleManager;
+        }
+
+        public ReadOnlyCollection<IVoteStatus> TallyResults()
+        {
+            if (!AmongUsClient.Instance.AmHost)
+                throw new InvalidOperationException("Only the host can tally results");
+
+            var results = new List<IVoteStatus>();
+            foreach (var role in RoleManager.Roles)
+            {
+                results.AddRange(role.GetVotes().Where(vote => vote != null && vote.IsRendered));
+            }
+
+            return results.AsReadOnly();
+        }
+
+        public void RenderResults()
+        {
+            var resultLists = TallyResults().GroupBy(vote => vote.Serialize())
+                .Select(group => group.ToList()).ToList();
+
+            foreach (var state in MeetingHud.Instance.playerStates)
+                state.ClearForResults();
+
+            var maxIndex = resultLists.Max(list => list.Count);
+            for (int index = 0; index < maxIndex; index++)
+            {
+                foreach (var list in resultLists)
+                {
+                    if (list.Count <= index)
+                    {
+                        continue;
+                    }
+
+                    var vote = list[index];
+                    AnimateVoteIcon(vote, index);
+                    list.RemoveAt(index);
+                }
+
+                resultLists.RemoveAll(list => list.Count <= index);
+            }
+        }
+
+        public void AnimateVoteIcon(IVoteStatus vote, int index)
+        {
+            var voteArea = MeetingHud.Instance.GetAllVoteAreas().Single(area => area.TargetPlayerId == vote.Serialize());
+            var player = GameData.Instance.GetPlayerById(vote.Serialize());
+
+            var voteIcon = GenerateVoteIcon(vote.IsAnonymous, player.ColorId, voteArea.transform);
+
+            MeetingHud.Instance.StartCoroutine(Effects.Bloop(0.3f * index, voteIcon.transform));
+            voteArea.transform.GetComponent<VoteSpreader>().AddVote(voteIcon);
+        }
+
+        private SpriteRenderer GenerateVoteIcon(bool isAnonymous, int? color, Transform parent)
+        {
+            var vote = UnityEngine.Object.Instantiate(MeetingHud.Instance.PlayerVotePrefab);
+
+            if (isAnonymous)
+            {
+                PlayerControl.SetPlayerMaterialColors(Palette.DisabledGrey, vote);
+            }
+            else
+            {
+                PlayerControl.SetPlayerMaterialColors(color!.Value, vote);
+            }
+
+            vote.transform.SetParent(parent);
+            vote.transform.localScale = Vector3.zero;
+            return vote;
+        }
+    }
+}
---
+++ source/VoteStatus.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using TownOfUs.Roles;
+
+namespace TownOfUs
+{
+    public interface IVoteStatus
+    {
+        public bool IsRendered { get; }
+        public bool IsAnonymous { get; }
+
+        public byte Serialize();
+    }
+
+    public class PlayerVoteStatus : IVoteStatus
+    {
+        public PlayerControl Player { get; }
+
+        public bool IsRendered => true;
+        public bool IsAnonymous { get; } = false;
+
+        public PlayerVoteStatus(PlayerControl player, bool isAnonymous = false)
+        {
+            Player = player ?? throw new ArgumentNullException(nameof(player));
+            IsAnonymous = isAnonymous;
+        }
+
+        public byte Serialize()
+        {
+            return Player.PlayerId;
+        }
+    }
+
+    public class SkipVoteStatus : IVoteStatus
+    {
+        public bool IsRendered => true;
+        public bool IsAnonymous { get; } = false;
+
+        public SkipVoteStatus(bool isAnonymous = false)
+        {
+            IsAnonymous = isAnonymous;
+        }
+
+        public byte Serialize()
+        {
+            return PlayerVoteArea.SkippedVote;
+        }
+    }
+
+    public class AbstainVoteStatus : IVoteStatus
+    {
+        public bool IsRendered => false;
+        public bool IsAnonymous => false;
+
+        public byte Serialize()
+        {
+            return MayorRole.AbstainPlayerId;
+        }
+    }
+
+    public class HasNotVotedStatus : IVoteStatus
+    {
+        public bool IsRendered => false;
+        public bool IsAnonymous => false;
+
+        public byte Serialize()
+        {
+            return PlayerVoteArea.HasNotVoted;
+        }
+    }
+}
